479285fdc1cf266573c17c98d82645c679bd5b4e
chirag Budhiraja <chiragbudhiraja005@gmail.com>
2020-06-19 10:32:25 +0530

    Updates

---
+++ precache-manifest.a37d91cee63b23900d48af5930a15811.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ebbd80a8d0ddfc0e613dd261581093",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "c8e17a91ac0c5039abdc",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c8b881286f93c4afab33",
+    "url": "/react-deploy/static/js/2.0e48ec02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.0e48ec02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e17a91ac0c5039abdc",
+    "url": "/react-deploy/static/js/main.f49ff570.chunk.js"
+  },
+  {
+    "revision": "78865c0d27ca50c9c42b",
+    "url": "/react-deploy/static/js/runtime-main.ad790cbc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
